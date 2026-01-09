0ad40e00d22e876a108dac76a469386f80b03539
connorglass01 <connorglass0@gmail.com>
2026-01-09 14:35:26 +1000

    welcome msg

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int welcome();
+
+int main()
+{
+    welcome();
+
+    while (true)
+    {
+        std::cout << "doin\'> ";
+
+        std::string command{};
+        std::getline(std::cin, command);
+
+        if (command == "exit")
+            break;
+        else
+            std::cout << "Command " << command << " is unknown\n";
+    }
+
+    //std::system("clear");
+
+    return 0;
+}
+
+int welcome()
+{
+    {
+        // Writing to the "whatsdoing.txt" file
+        std::ofstream outf { "whatsdoing.txt"};
+        if (!outf)
+            {
+                std::cout << "Something went wrong \n";
+                return 1;
+            }    
+        
+        std::string welcome {
+            "~~~ Welcome to whatsdoing ~~~\n"
+        };
+        outf << welcome;
+    }   // outf goes out of scope (equiv. to outf.close())
+
+    // Reading the "whatsdoing.txt" file
+    std::ifstream inf { "whatsdoing.txt" };
+    if (!inf)
+    {
+        std::cout << "something went wrong reading\n";
+        return 1;
+    }
+
+    std::string strInput{};
+    while (std::getline(inf, strInput))
+    {
+        std::cout << strInput << '\n';
+    }
+
+    return 0;
+}
